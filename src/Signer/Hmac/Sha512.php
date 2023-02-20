20c68cf6bd4cfac770cd21224bc0ac2c7a8adf6e
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-02-20 22:56:34 +0800

    Update Sha512.php

--- src/Signer/Hmac/Sha512.php
+++ src/Signer/Hmac/Sha512.php
@@ -7,18 +7,4 @@ use Lcobucci\JWT\Signer\Hmac;
 
 final class Sha512 extends UnsafeSha512
 {
-    public function algorithmId(): string
-    {
-        return 'HS512';
-    }
-
-    public function algorithm(): string
-    {
-        return 'sha512';
-    }
-
-    public function minimumBitsLengthForKey(): int
-    {
-        return 512;
-    }
 }
